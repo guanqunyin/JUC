287fbad955d7eda03eef56b2bda7e6b8974b0835
guanqunyin <1151900425@qq.com>
2019-11-18 21:04:57 +0800

    第一天内容回顾

---
+++ src/com/yin/juc/c_004/TestSync.java
@@ -0,0 +1,12 @@
+package com.yin.juc.c_004;
+
+/**
+ * Created by Administrator on 2019/11/18.
+ */
+public class TestSync {
+
+    public static void main(String[] args) {
+        Integer integer = new Integer(3);
+//        integer.
+    }
+}
