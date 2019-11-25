31ccf38d6adb17234bc5481d7df1218cab33397a
guanqunyin <1151900425@qq.com>
2019-11-25 18:15:43 +0800

    更新一些内容

--- src/com/yin/juc/day2/day2_005_AtomicXxx/TestAtomic.java
+++ src/com/yin/juc/day2/day2_005_AtomicXxx/TestAtomic.java
@@ -1,11 +1,22 @@
 package com.yin.juc.day2.day2_005_AtomicXxx;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * TODO 写一个程序，证明AtomXXX类比synchronized更高效.
  */
 public class TestAtomic {
 
-    void m(){
+    AtomicInteger number;
 
+    void m(){
+        for (int i = 0; i < 10000; i++) {
+            number.incrementAndGet();
+        }
+    }
+
+    public static void main(String[] args) {
+        TestAtomic atomic = new TestAtomic();
+        new Thread(atomic::m);
     }
 }
