4c7fbca672c68aeb84a45919bae30194f1c0177a
guanqunyin <1151900425@qq.com>
2019-11-27 23:22:11 +0800

    volatile 以及读写锁

--- src/com/yin/juc/day3/T09_TestReadWriteLock.java
+++ src/com/yin/juc/day3/T09_TestReadWriteLock.java
@@ -1,7 +1,27 @@
 package com.yin.juc.day3;
 
+import java.util.LinkedList;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /**
  * 共享锁和排它锁 TODO 自己测试一下怎么用
  */
-public class T09_TestReadWriteLock {
+public class T09_TestReadWriteLock<E> {
+
+    ReadWriteLock lock = new ReentrantReadWriteLock();
+
+    LinkedList<E> linkedList = new LinkedList();
+
+    void add(E e) {
+
+        linkedList.addLast(e);
+    }
+
+    void get() {
+        linkedList.getFirst();
+    }
+
 }
+
+
