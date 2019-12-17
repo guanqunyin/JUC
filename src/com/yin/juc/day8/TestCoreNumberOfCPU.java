79393f5bddd6708a53c3fa6f177b8d7a30f59f21
guanqunyin <1151900425@qq.com>
2019-12-17 23:12:28 +0800

    day08笔记

---
+++ src/com/yin/juc/day8/TestCoreNumberOfCPU.java
@@ -0,0 +1,11 @@
+package com.yin.juc.day8;
+
+/**
+ * Created by Administrator on 2019/12/17.
+ */
+public class TestCoreNumberOfCPU {
+
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
